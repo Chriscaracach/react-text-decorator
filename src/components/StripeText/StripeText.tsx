acedd1d2c27443793ce0711bf91143143b5e2a0c
Christian Caracach <christiancaracach@gmail.com>
2024-03-16 20:49:36 -0300

    docs: added comment

--- src/components/StripeText/StripeText.tsx
+++ src/components/StripeText/StripeText.tsx
@@ -4,6 +4,7 @@ import type { StripeTextProps } from "../../interfaces/StripeText";
 import StripeDecorator from "./decorator/StripeDecorator";
 
 // TODO Add left/right-decorator feature
+// TODO Add Size feature
 // TODO See what are we going to do with the "rtd-stripe" classname
 
 export const StripeText = ({
