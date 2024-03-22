cbcf94ebffe41bad4ef10f1ea15ff29c488ace22
Christian Caracach <christiancaracach@gmail.com>
2024-03-21 22:08:16 -0300

    test: added new tests for new features

--- tests/StripeText.spec.tsx
+++ tests/StripeText.spec.tsx
@@ -1,6 +1,5 @@
 // Import necessary dependencies
 import "@testing-library/jest-dom/extend-expect";
-import "../src/components/StripeText/StripeText.scss";
 
 import { render, screen } from "@testing-library/react";
 import React from "react";
@@ -13,26 +12,33 @@ describe("StripeText component", () => {
 
 		// Ensure the component is rendered
 		expect(screen.getByTestId("stripe-text")).toBeInTheDocument();
-
-		// Ensure the default color class is applied
-		expect(screen.getByTestId("stripe-text")).toHaveClass("rtd-stripe");
-
-		// Ensure the text is rendered
-		expect(screen.getByTestId("stripe-text")).toBeInTheDocument();
 	});
 
 	test("renders with custom props", () => {
 		const customText = "Custom Text";
 		const customColor = "error";
 		const customClassname = "custom-class";
+		const customSize = "sm";
 
-		render(<StripeText text={customText} color={customColor} customClassname={customClassname} />);
+		render(
+			<StripeText
+				text={customText}
+				color={customColor}
+				size="sm"
+				customClassname={customClassname}
+			/>
+		);
 
 		// Ensure the component is rendered
 		expect(screen.getByTestId("stripe-text")).toBeInTheDocument();
 
 		// Ensure the custom color class is applied
-		expect(screen.getByTestId("stripe-text")).toHaveClass(`rtd-stripe ${customClassname}`);
+		expect(screen.getByTestId("stripe-text")).toHaveClass(`${customClassname}`);
+
+		// Ensure the decorator receives custom size
+		expect(screen.getByTestId("stripe-decorator-1")).toHaveClass(
+			`rtd-stripe-decorator-1 rtd-stripe-decorator-color-error rtd-stripe-decorator-size-${customSize}`
+		);
 
 		// Ensure the custom text is rendered
 		expect(screen.getByText(customText)).toBeInTheDocument();
